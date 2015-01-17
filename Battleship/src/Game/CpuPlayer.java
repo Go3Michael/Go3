259e8aba9ae7e4ed5b5b0d01e9802120995ac533
Go3Michael <michael.mammut@gmail.com>
2015-01-17 18:10:47 +0100

    ÄnderungsTest

--- Battleship/src/Game/CpuPlayer.java
+++ Battleship/src/Game/CpuPlayer.java
@@ -1,7 +1,22 @@
 package Game;
 
+import java.util.Random;
+
 public class CpuPlayer 
 {
-	//MICHI
 
+
+    protected int cpuMove(){
+	int move = 0;
+	int hightPos = 0;
+	int widthPos = 0;
+	
+	Random posRandom  = new Random();
+	
+	
+	hightPos = posRandom.nextInt(10);
+	
+	
+	return move;
+    }
 }
