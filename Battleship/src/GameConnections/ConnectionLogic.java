b17d806f1b065019ba7fe24e7bccd800436b5ffe
purkart <julianpurkart@gmail.com>
2015-01-18 12:57:15 +0100

    added ConnectionLogic

---
+++ Battleship/src/GameConnections/ConnectionLogic.java
@@ -0,0 +1,32 @@
+package GameConnections;
+
+import GameUtilities.Command;
+
+public class ConnectionLogic
+{
+	Connection connection;
+
+	public ConnectionLogic(Connection connection)
+	{
+		this.connection = connection;
+		
+	}
+	
+	public void sendCommandToPlayer(Command commandSend)
+	{
+		this.connection.sendCommand(commandSend);
+	}
+	
+	public Command getCommandFromPlayer()
+	{
+		Command commandRecieve = this.connection.receiveCommand();
+		
+		return commandRecieve;
+	}
+	
+	public void closeConnection()
+	{
+		this.connection.close();
+	}
+	
+}
