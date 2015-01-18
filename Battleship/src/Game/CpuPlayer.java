e380b0edc1982a5844de88817a5e2273497b88d1
Go3Michael <michael.mammut@gmail.com>
2015-01-18 20:29:53 +0100

    Implementierung des CPU-Players, und benötigter Files

--- Battleship/src/Game/CpuPlayer.java
+++ Battleship/src/Game/CpuPlayer.java
@@ -1,22 +1,135 @@
 package Game;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class CpuPlayer 
+import GameUtilities.Ship;
+import GameUtilities.ShipPosition;
+import GameUtilities.ShipType;
+import GameUtilities.ShipAlignment;
+import GameUtilities.Field.Field;
+
+public class CpuPlayer
 {
+    private Field field = new Field();
 
+    private Field fieldWithSettings;
+    private List<Point> enemyShot = new ArrayList<Point>();
+    private List<Point> cpuShot = new ArrayList<Point>();
+    private int size_ShipTypeEnum = ShipType.values().length;
+    private int size_ShipAlignmentEnum = ShipAlignment.values().length;
+    private Point actShot;
+    
+    
+    
+    public void test()
+    {
 
-    protected int cpuMove(){
-	int move = 0;
-	int hightPos = 0;
-	int widthPos = 0;
-	
-	Random posRandom  = new Random();
 	
 	
-	hightPos = posRandom.nextInt(10);
-	
-	
-	return move;
+	Point point = new Point();
+	System.out.println(this.cpuShot.toString());
+	System.out.println("Schuss: " + shotAtEnemie());
+	System.out.println(this.cpuShot.toString());
+	System.out.println("ende");
     }
+
+    // geprüft
+    public Point shotAtEnemie()
+    {
+	
+	boolean[] shotFlag = new boolean[2];
+	shotFlag[0] = true;
+	shotFlag[1] = true;
+
+	this.actShot = cpuRandomShot();
+
+	while ((shotFlag[0] == true) && (shotFlag[1] == true))
+	{
+
+	    for (Point shot : this.cpuShot)
+	    {
+		if (shot.equals(this.actShot))
+		{
+		    shotFlag[1] = false;
+		    break;
+		}
+	    }
+	    this.cpuShot.add(this.actShot);
+	}
+	return this.actShot;
+    }
+
+    private void setShip()
+    {
+	int posX = this.getRandomValue(10);
+	int posY = this.getRandomValue(10);
+
+	int shipAlignmentRand = this.getRandomValue(size_ShipAlignmentEnum);
+	String alignment = this.getShipTypeAlignment(shipAlignmentRand);
+
+	Point point = new Point(posX, posY);
+	ShipPosition position = new ShipPosition(point, alignment);
+
+	int shipTypeRand = this.getRandomValue(size_ShipTypeEnum);
+	Ship ship = new Ship(position, ShipType.values()[shipTypeRand], 1);
+
+	Field field = new Field();
+	field.setShipOnField(ship);
+
+    }
+
+    /**
+     * Ermittelt einen Zufallszahl die größer als 0 ist und im Bereich vom übergebenen Wert.
+     * 
+     * @param integer valueRange
+     * @return integer
+     */
+    private int getRandomValue(int valueRange)
+    {
+
+	if (valueRange == 0)
+	{
+	    ++valueRange;
+	}
+	int returnValue = 0;
+	Random rand = new Random();
+	returnValue = rand.nextInt(valueRange) + 1;
+
+	return returnValue;
+    }
+
+    /**
+     * Gibt die Tye des Schiffes zurück
+     * 
+     * @param integer index
+     * @return string
+     */
+    private String getShipTypeAlignment(int index)
+    {
+	String shipAlignmentString;
+	ShipAlignment shipAlignmentValue;
+	shipAlignmentValue = ShipAlignment.values()[index];
+
+	shipAlignmentString = shipAlignmentValue.toString();
+
+	return shipAlignmentString;
+    }
+
+    /**
+     * Setzt einen Schuss mit dem Zufallsgenerator ab
+     * 
+     * @return
+     */
+    private Point cpuRandomShot()
+    {
+	int posX = this.getRandomValue(10);
+	int posY = this.getRandomValue(10);
+	Point setShot = new Point(posX, posY);
+
+	return setShot;
+    }
+
 }
