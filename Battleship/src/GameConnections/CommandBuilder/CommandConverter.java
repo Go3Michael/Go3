eb1e98bdde69fcdf621bbed151091d55c4347380
purkart <julianpurkart@gmail.com>
2015-01-18 12:54:22 +0100

    Refactored ConvertCommandToTCPString to CommandConverter

---
+++ Battleship/src/GameConnections/CommandBuilder/CommandConverter.java
@@ -0,0 +1,28 @@
+package GameConnections.CommandBuilder;
+
+import GameUtilities.Command;
+
+public class CommandConverter 
+{
+	
+	public CommandConverter()
+	{
+		
+	}
+	
+	public String convertToTCPString(Command command)
+	{
+		//TODO
+		String string = "";
+		return string;
+	}
+	
+	public Command convertToGameCommand(String commandString)
+	{
+		//TODO string to command
+		Command convertCommand = new Command(0, null, "");
+		
+		return convertCommand;
+	}
+	
+}
