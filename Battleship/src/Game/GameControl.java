0c18f32d93d3cb46482bb61c5fb86b712e89df88
purkart <julianpurkart@gmail.com>
2015-01-23 12:18:44 +0100

    Clean up and doc

--- Battleship/src/Game/GameControl.java
+++ Battleship/src/Game/GameControl.java
@@ -1,18 +1,18 @@
 package Game;
+
 import GameConnections.*;
 
-public class GameControl 
+/**
+ * @author Schoenegger / Purkart / Koch
+ */
+public class GameControl
 {
 	private boolean shipsInPosition = false;
 	private Connection connection;
-	
+
 	public boolean checkConnectionState()
 	{
 		return false;
 	}
-	
-	
-	
-	
-	
+
 }
